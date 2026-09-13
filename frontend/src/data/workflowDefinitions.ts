51e9e8bbc562e48893abc40f7f7762b971212a0d
mtoganba <tfofebruary@gmail.com>
2026-09-14 00:06:28 +0500

    Добавление этапа голосования членов АК

--- frontend/src/data/workflowDefinitions.ts
+++ frontend/src/data/workflowDefinitions.ts
@@ -4,6 +4,7 @@ export const STEPS = [
   ["requested", "Позиция ДВГА"],
   ["materials", "Справка по доводам"],
   ["circulated", "Позиции членов комиссии"],
+  ["commission_voting", "Голосование членов АК"],
   ["hearing", "Заслушивание"],
   ["meeting", "Заседание и голосование"],
   ["protocol", "Протокол и решение"],
